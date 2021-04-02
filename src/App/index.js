8fe59ccc271d4a5d302c6e2feb191b1711ef7db5
PeterYama <35582343+PeterYama@users.noreply.github.com>
2021-04-02 18:59:16 +1100

    added styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,28 @@
+import React from 'react'
 import './App.css';
+import Welcome from './welcome'
+import styled, {css} from 'styled-components'
+
+//Simple component create with styled components
+const MyButtons = styled.div`
+  color:green
+  ${props => props.primary && css`
+    background:white,
+  `}
+`
+//Extending and modifying an exsisting style
+const TomatoButton = styled(MyButtons)`
+  color:tomato;
+  border-color: tomato;
+`
 
 function App() {
   return (
     <div className="App">
-      <div>Hello World</div>
+      <Welcome name={"Peter"}/>
+      <MyButtons> Hello </MyButtons>
+      <MyButtons primary> Hello </MyButtons>
+      <TomatoButton>Tomato</TomatoButton>
     </div>
   );
 }
