78094d81b1242041b4d962dd0e76cd779c095135
PeterYama <35582343+PeterYama@users.noreply.github.com>
2021-04-05 16:23:08 +1000

    added coin grid

--- src/App/settings/index.js
+++ src/App/settings/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Welcome from './welcome'
 import ConfirmButtom from './ConfirmButtom'
 import Page from './Shared/Page'
+import CoinGrid from './CoinGrid'
 
 export default function index() {
     return (
         <Page name='Settings'>
             <Welcome />
             <ConfirmButtom />
+            <CoinGrid />
         </Page>
     )
 }
