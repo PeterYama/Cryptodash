c947db596d094fa00b075aa969c1836a7376ddd6
PeterYama <35582343+PeterYama@users.noreply.github.com>
2021-04-05 16:11:24 +1000

    added loading content

--- src/App/index.js
+++ src/App/index.js
@@ -5,13 +5,16 @@ import AppLayout from './AppLayout'
 import AppBar from './AppBar'
 // Wrap the main component with AppProvider so Inner components can use Consume the context
 import AppProvider from './AppProvider'
+import Content from './settings/Shared/Content'
 
 function App() {
   return (
     <AppLayout>
       <AppProvider>
         <AppBar/>
+        <Content>
           <Welcome />
+        </Content>
       </AppProvider>
     </AppLayout>
   );
---
+++ src/App/settings/Shared/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AppContext} from '../../AppProvider'
+
+export default function Content(props) {
+    return (
+        <AppContext.Consumer>
+            {({coinList}) => {
+                if(!coinList){
+                    return <div>Loading coins</div>
+                }
+                return <div>{props.children}</div>
+            }}
+        </AppContext.Consumer>
+    )
+}
